06ba28370944a609a6dda451d3ec0a857b037356
rlesieur <10141113+rlesieur@users.noreply.github.com>
2023-11-13 11:36:08 +0100

    Add files via upload

---
+++ ParallelAndNarrowChange/Tests/AuthenticatorShould.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelAndNarrowChange.Method;
+
+namespace ParallelAndNarrowChange.Tests{
+    [TestClass]
+    public class AuthenticationServiceTest{
+        [TestMethod]
+        public void administrator_is_always_authenticated(){
+            var service = new AuthenticationService();
+            var adminId = 12345;
+            Assert.IsTrue(service.IsAuthenticated(adminId));
+        }
+
+        [TestMethod]
+        public void normalUser_is_not_authenticated_initially(){
+            AuthenticationService service = new AuthenticationService();
+            int normalUserId = 11111;
+            Assert.IsFalse(service.IsAuthenticated(normalUserId));
+        }
+    }
+}
---
+++ ParallelAndNarrowChange/Tests/ShoppingCartShould.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelAndNarrowChange.Field;
+
+namespace ParallelAndNarrowChange{
+    [TestClass]
+    public class ShoppingCartShould{
+        private ShoppingCart cart;
+
+        [TestInitialize]
+        public void SetUp(){
+            cart = new ShoppingCart();
+        }
+
+        [TestMethod]
+        public void calculate_the_final_price(){
+            cart.Add(10);
+
+            Assert.AreEqual(10, cart.CalculateTotalPrice());
+        }
+
+        [TestMethod]
+        public void knows_the_number_of_items(){
+            cart.Add(10);
+
+            Assert.AreEqual(1, cart.NumberOfProducts());
+        }
+
+        [TestMethod]
+        public void may_offer_discounts_when_there_at_least_one_expensive_product(){
+            cart.Add(120);
+
+            Assert.IsTrue(cart.HasDiscount());
+        }
+
+        [TestMethod]
+        public void does_not_offer_discount_for_cheap_products(){
+            cart.Add(10);
+
+            Assert.IsFalse(cart.HasDiscount());
+        }
+    }
+}
